7400163ea44dbc46300b6f8df646f7b211e6e6d6
Blake_Whitham <bwhitham77@gmail.com>
2021-04-01 13:05:16 -0700

    added send email route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 /* eslint-disable no-console */
+require('dotenv').config();
 const expressStaticGzip = require('express-static-gzip');
 
 const express = require('express');
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const pass = process.env.GMAIL_PASS;
+const user = process.env.GMAIL_USER;
+
+
+
+app.use(cors());
+app.use(express.json());
 app.use(expressStaticGzip('client', {
   enableBrotli: true,
   orderPreference: ['br', 'gz'],
@@ -14,4 +24,42 @@ app.use(expressStaticGzip('client', {
   },
 }));
 
+const contactEmail = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  auth: {
+    user,
+    pass,
+  },
+});
+
+app.post('/contact', (req, res) => {
+  const name = req.body.name;
+  const subject = req.body.subject;
+  const email = req.body.email;
+  const message = req.body.message;
+  const mail = {
+    from: name,
+    to: process.env.GMAIL_USER,
+    subject,
+    html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "failed" });
+    } else {
+      res.json({ status: "sent" });
+    }
+  });
+})
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
 app.listen(PORT, () => { console.log(`listening on port: ${PORT}`); });
+
